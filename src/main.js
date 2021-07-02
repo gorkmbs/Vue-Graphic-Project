538878a3587630d2c9e96b60dacf2e6f995463e7
GörkemSaraç <tamzirtapoz@hotmail.com>
2021-07-02 23:04:59 +0300

    overall changes

--- src/main.js
+++ src/main.js
@@ -3,16 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/sass/index.sass";
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
+// import Vue from "vue";
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// // Import Bootstrap an BootstrapVue CSS files (order is important)
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// // Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue);
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin);
 
 createApp(App).use(store).use(router).mount("#app");
