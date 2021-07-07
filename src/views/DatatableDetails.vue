91d598d5b7badd44c4849d168631aff939899a9b
GörkemSaraç <tamzirtapoz@hotmail.com>
2021-07-07 12:54:57 +0300

    bug fix for prevent viewing details page when user logged out

--- src/views/DatatableDetails.vue
+++ src/views/DatatableDetails.vue
@@ -6,19 +6,34 @@
       left: '5px',
       zIndex: '100',
     }"
+    v-if="loginStatus"
   >
     <button class="btn btn-secondary" @click="backToDatatablePage">Geri</button>
   </div>
   <div
     class="d-flex justify-content-center align-items-center"
     :style="{ height: '100vh' }"
+    v-if="loginStatus"
   >
     <h1>Detail Page For {{ detailPageFor.th }}</h1>
+  </div>
+  <div
+    class="d-flex justify-content-center align-items-center"
+    :style="{ height: '100vh' }"
+    v-if="!loginStatus"
+  >
+    <div class="container">
+      <h1>Not logged in</h1>
+      <button class="btn btn-primary" @click="backToLoginScreen">
+        GİRİŞ EKRANINA DÖN
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import datatableItems from "@/data/datatableItems";
+import { mapState } from "vuex";
 
 export default {
   data() {
@@ -30,6 +45,9 @@ export default {
     backToDatatablePage() {
       this.$router.push("/dashboard");
     },
+  },
+  computed: {
+    ...mapState(["loginStatus"]),
   },
 };
 </script>
